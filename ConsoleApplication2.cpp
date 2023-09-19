cd8dce4229c29a9eea8d7aa919357da75a76816d
OtterCuttie <144522961+OtterCuttie@users.noreply.github.com>
2023-09-19 20:24:04 +1000

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	int a;
+	int b;
+	int c;
+	cout << "Введите три угла в градусах";
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	if (a == b or a == c or c == b){
+		cout << "Равнобедренный треуголтник";
+	    if (a == b and a == c and b == c) {
+		cout << "Равносторонний треугольник";
+	    }
+	}
+	else if (a ==90 or b==90 or c==90) {
+		cout << "Прямоугольный треуголтник";
+		if (a == b or a == c or c == b) {
+			cout << "Равнобедренный треуголтник";
+		}
+     
+	}
+    else if (a == b and a == c and b == c) {
+			cout << "Равносторонний треугольник";
+		}
+	else if (a+b+c!=180 ) {
+		cout << "Треугольник не получается";
+	}
+	else if (a <0 or c<0 or b<0) {
+		cout << "Треугольник не получается";
+	}
+	else if (a ==180  or c==180 or b ==180) {
+		cout << "Треугольник не получается";
+	}
+	else if (a > 180 or c > 180 or b > 180) {
+		cout << "Треугольник не получается";
+	}
+	else{
+		cout << "Введите нормально числа";
+	}
+}
